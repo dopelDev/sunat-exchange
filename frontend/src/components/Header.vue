1f8dcbfa08d5a6ba8700075c112ea9f7d22aabea
dopeldev <322kuroneko@gmail.com>
2024-02-06 01:33:06 -0500

    add composables

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+import { useDeviceType } from '@/composables/toBeOrNot.js'
+const { deviceType } = useDeviceType()
+	export default {
+		name: 'Header',
+		data() {},
+	}
+</script>
+
+<style>
+
+</style>
