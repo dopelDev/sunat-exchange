e52c5f5d1d2b9a2ea3c22aaf04ecec596516864f
dopeldev <322kuroneko@gmail.com>
2024-02-12 00:20:07 -0500

    add flask-swagger

--- api/api.py
+++ api/api.py
@@ -1,9 +1,24 @@
 from flask_restful import Api, Resource
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 from methods import sunatscrapy
 
 app = Flask(__name__)
 api = Api(app, catch_all_404s=True)
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+
+# Configuración de Flask-Swagger-UI
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # URL de Swagger UI
+    API_URL,  # URL del archivo JSON de Swagger
+    config={
+        'app_name': "Ejemplo de API Flask"
+    },
+)
+
+# Registrar el blueprint de Swagger UI en la aplicación Flask
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 date, compra, venta = sunatscrapy.get_rate()
 
